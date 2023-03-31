89fa32c00226152ca043ba27ddaabd15099b4448
Saint Jesus <89384211+SaintJesus@users.noreply.github.com>
2023-03-31 12:21:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+import { ProfilePage } from "./pages/Profile";
+import { SettingsPage } from "./pages/Settings";
+import { ProtectedLayout } from "./components/ProtectedLayout";
+import { HomeLayout } from "./components/HomeLayout";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<HomeLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="*" element={<Navigate to="/login" />} /> */}
+      </Route>
+
+      <Route path="/dashboard" element={<ProtectedLayout />}>
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  );
+}
